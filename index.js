d0c61380f22944a209c1ff5cce49e23cf0dee7ec
xxshamanchikxx <svsmoliar@gmail.com>
2026-01-05 16:04:18 +0200

    Fix CORS for production

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: '*',
+    origin: process.env.CORS_ORIGIN || '*',
     methods: ['GET', 'POST']
   }
 });
